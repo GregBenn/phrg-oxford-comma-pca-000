2f88d07d64ad2ab987c53ba1e976f41fba0aef70
GregBenn <gbenn07@gmail.com>
2018-05-15 11:18:33 -0400

    oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length <= 2
+    array.join(" and ")
+  else
+    # join with a comma and space between each item
+    array[-1] = "and #{array[-1]}"
+    # binding.pry
+
+    array.join(", ")
+    # Add an 'and' before last item
+  end
+# binding.pry
 end
